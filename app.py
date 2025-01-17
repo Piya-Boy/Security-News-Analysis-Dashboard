54c189be2c17b8d84ff910662adb6004b5424cc1
Piya Miang-Lae <87295280+Piya-Boy@users.noreply.github.com>
2025-01-17 21:01:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def load_data_from_api():
         # แปลงคอลัมน์ Date เป็น datetime
         df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
         
-        df["Date"] = df["Date"].apply(lambda x: datetime.strptime(x, df["Date"]) if len(x) == 12 else datetime.strptime(x, "%B %d, %Y"))
+        df["Date"] = df["Date"].apply(lambda x: datetime.strptime(x, "%b %d, %Y") if len(x) == 12 else datetime.strptime(x, "%B %d, %Y"))
         
         # เพิ่มคอลัมน์ Month และ Year
         df["Month"] = df["Date"].dt.strftime("%B") 
